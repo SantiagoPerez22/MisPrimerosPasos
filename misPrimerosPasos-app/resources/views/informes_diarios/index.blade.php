fd7391bd7a0c087e84dfb26ce8e90aae92672f63
EQM <78054560+ElQuesoMagico@users.noreply.github.com>
2024-07-04 22:20:06 -0400

    Cambios a botones
    Cambios a botones en vista index de informe diario
    Texto => Iconos

--- misPrimerosPasos-app/resources/views/informes_diarios/index.blade.php
+++ misPrimerosPasos-app/resources/views/informes_diarios/index.blade.php
@@ -2,37 +2,56 @@
 
 @section('content')
 <div class="container">
-    <h1>Informes Diarios</h1>
-    <a href="{{ route('informes_diarios.create') }}" class="btn btn-primary">Agregar Informe Diario</a>
-    <table class="table mt-3">
-        <thead>
-        <tr>
-            <th>ID</th>
-            <th>Condición</th>
-            <th>Alumno</th>
-            <th>Fecha</th>
-            <th>Acciones</th>
-        </tr>
-        </thead>
-        <tbody>
-        @foreach($informesDiarios as $informeDiario)
-        <tr>
-            <td>{{ $informeDiario->id }}</td>
-            <td>{{ $informeDiario->condicion->nombre }}</td>
-            <td>{{ $informeDiario->alumno->alumno->nombre1 }} {{ $informeDiario->alumno->alumno->apellido1 }}</td>
-            <td>{{ $informeDiario->fecha }}</td>
-            <td>
-                <a href="{{ route('informes_diarios.show', $informeDiario->id) }}" class="btn btn-info">Ver</a>
-                <a href="{{ route('informes_diarios.edit', $informeDiario->id) }}" class="btn btn-warning">Editar</a>
-                <form action="{{ route('informes_diarios.destroy', $informeDiario->id) }}" method="POST" style="display:inline;">
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit" class="btn btn-danger">Eliminar</button>
-                </form>
-            </td>
-        </tr>
-        @endforeach
-        </tbody>
-    </table>
+    <!-- Table Element -->
+    <div class="card border-0">
+        <div class="card-header">
+            <h5 class="card-title">
+                Informes Diarios
+            </h5>
+            <h6 class="card-subtitle text-muted">
+                Registro de informes diarios
+            </h6>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-hover">
+                    <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Condición</th>
+                        <th scope="col">Alumno</th>
+                        <th scope="col">Fecha</th>
+                        <th scope="col">Acciones</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @foreach($informesDiarios as $informeDiario)
+                    <tr>
+                        <th scope="row">{{ $informeDiario->id }}</th>
+                        <td>{{ $informeDiario->condicion->nombre }}</td>
+                        <td>{{ $informeDiario->alumno->alumno->nombre1 }} {{ $informeDiario->alumno->alumno->apellido1 }}</td>
+                        <td>{{ $informeDiario->fecha }}</td>
+                        <td>
+                            <a href="{{ route('informes_diarios.show', $informeDiario->id) }}" class="btn btn-sm btn-info">
+                                <i class="fa fa-eye"></i>
+                            </a>
+                            <a href="{{ route('informes_diarios.edit', $informeDiario->id) }}" class="btn btn-sm btn-warning">
+                                <i class="fa fa-pencil"></i>
+                            </a>
+                            <form action="{{ route('informes_diarios.destroy', $informeDiario->id) }}" method="POST" style="display:inline;">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-sm btn-danger">
+                                    <i class="fa fa-trash"></i>
+                                </button>
+                            </form>
+                        </td>
+                    </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
